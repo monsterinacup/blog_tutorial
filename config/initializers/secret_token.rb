6b8671ef3a1f6f753cfd07dda6942d93dd130d87
monsterinacup <h.n.trewhitt@gmail.com>
2013-06-15 22:07:28 +0100

    1st Version - Step 4
    database created, server can run

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogTutorial::Application.config.secret_token = 'b5da1da04e55311c67114f2d67e06fc263127015b4757c68b30e3ca664b01f58be356774bf037ecf299818ad6947c792b2d9447b1bae9fef28dd4ba3dc841e9a'
